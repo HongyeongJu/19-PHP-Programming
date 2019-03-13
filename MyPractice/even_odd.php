f53760671686fb2fed9f24484ba38dfae03af728
Hong Yeong Ju <abc123472000@naver.com>
2019-03-14 00:48:23 +0900

    UPDATE myPractice

---
+++ MyPractice/even_odd.php
@@ -0,0 +1,8 @@
+<?php
+    $num = 80;
+    if($num%2 == 0){
+        echo "$num : Â¦¼ö";
+    }
+    else {
+        echo "$num : È¦¼ö";
+    }
---
+++ MyPractice/food_fee.php
@@ -0,0 +1,28 @@
+<?php
+    $a = 3;
+
+    switch($a) {
+        case 1:
+            echo "$a 학년 급식비 :  3만원 ";
+            break;
+        case 2:
+            echo "$a 학년 급식비 : 3만5천원 ";
+            break;
+        case 3:
+            echo "$a 학년 급식비 : 4만 원";
+            break;
+        case 4:
+            echo "$a 학년 급식비 : 4만 5천원";
+            break;
+        case 5:
+            echo "$a 학년 급식비  : 5만 원";
+            break;
+        case 6:
+            echo "$a 학년 급식비 :  5만 5천원";
+            break;
+        default :
+            echo "학년을 잘못 입력했어요!!!!";
+            break;
+    }
+    ?>
+
