0d2ce2c17915641df2d150d0c95aff0509ef3482
Hong Yeong Ju <abc123472000@naver.com>
2019-04-01 21:07:53 +0900

    UPDATE board.php by 홍영주 2014136137

--- boardz-master/board.php
+++ boardz-master/board.php
@@ -1,24 +1,21 @@
 <!-- 구글 검색 : galley board css => CSS Only Pinterest-like Responsive Board Layout - Boardz.css | CSS ... -->
 <!-- 출처 : https://www.cssscript.com/css-pinterest-like-responsive-board-layout-boardz-css/ -->
 
-<!-- 작성자 : 홍영주 컴퓨터 공학부 학번 : 2014136137 -->
 <!-- select * from 테이블명 where title like "%su%"; -->
 <?php
-    $connect = mysql_connect("localhost","hyj","1234");
-    $db_con = mysql_select_db("hyj_db", $connect);
-    if($_POST[search]){
-        $sql = "select * from boardz where title like \"%$_POST[search]%\";";
-    }else {
-        $sql = "select * from boardz;";
-    }
-    // 이건 db에 접속해서 db의 데이터를 꺼내는 것
-    $result = mysql_query($sql, $connect);
+$connect = mysql_connect("localhost","hyj","1234"); // MySQL 데이터베이스 연동
+$db_con = mysql_select_db("hyj_db", $connect);
+if($_POST[search]){                     //board.php(본 php파일에서) POST로 전달된 검색 변수 값을 가져옴
+    $sql = "select * from boardz where title like \"%$_POST[search]%\";";   //전달 받은 변수 값 search를 이용해서 select 쿼리 스트링 작성
+}else {
+    $sql = "select * from boardz;";                 //만약 search값이 없다면 boardz에 있는 전체 데이터 출력
+}
+// 이건 db에 접속해서 db의 데이터를 꺼내는 것
+$result = mysql_query($sql, $connect);              //mysql_query() 함수를 이용해서 ,MySQL 에 쿼리 스트링 전송
 
-    // mysql_num_rows 방법을 이용해서 row의 길이를 알아낸다.
+// mysql_num_rows 방법을 이용해서 row의 길이를 알아낸다.
 
-    $records = mysql_num_rows($result);
-    $fields = mysql_num_fields($result);
-
+$records = mysql_num_rows($result);                 //select 쿼리를 처리한 결과의 레코드의 수를 mysql_num_row()함수를 이용해서 구함
 ?>
 
 
@@ -26,7 +23,7 @@
 
 <html lang="en">
 <head>
-    <meta charset="UTF-8"> 
+    <meta charset="UTF-8">
 
     <title>Boardz Demo</title>
     <meta name="description" content="Create Pinterest-like boards with pure CSS, in less than 1kB.">
@@ -41,62 +38,63 @@
 </head>
 
 <body>
-    <div class="seventyfive-percent  centered-block">
-        <!-- Sample code block -->
-        <div>    
-            <hr class="seperator">
+<div class="seventyfive-percent  centered-block">
+    <!-- Sample code block -->
+    <div>
+        <hr class="seperator">
 
-            <!-- Example header and explanation -->
-            <div class="text-center">
-                <h2>Beautiful <strong>Boardz</strong></h2>
-                <div style="display: block; width: 50%; margin-right: auto; margin-left: auto; position: relative;">
-                    <form class="example" action="board.php" method="post">
-                        <input type="text" placeholder="Search.." name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-            </div>
-
-            <!--<hr class="seperator fifty-percent">-->
-
-            <!-- Example Boardz element. -->
-            <div class="boardz centered-block beautiful">
-                <?php
-                $count = ($records % 3);
-                if($records > 3){
-                    $column = 3;
-                }else {
-                    $column = $records;
-                }
-                for($i = 0 ; $i < $column ; $i++){
-                    echo "<ul>";
-                    for($j = 1; $j <($records / 3) ; $j++){
-                        if($row = mysql_fetch_array($result)){
-                            echo "<li>\n";
-                            echo "<h1>$row[title]</h1>\n";
-                            echo "$row[contents]\n";
-                            echo "<img src='$row[image_url]' alt='demo image'>\n";
-                            echo "</li>\n";
-                        }
-                    }
-                    if($count > 0 || $records == 3){
-                        if($row = mysql_fetch_array($result)){
-                            echo "<li>\n";
-                            echo "<h1>$row[title]</h1>\n";
-                            echo "$row[contents]\n";
-                            echo "<img src='$row[image_url]' alt='demo image'>\n";
-                            echo "</li>\n";
-                        }
-                        $count--;
-                    }
-                    echo "</ul>";
-                }
-                ?>
+        <!-- Example header and explanation -->
+        <div class="text-center">
+            <h2>Beautiful <strong>Boardz</strong></h2>
+            <div style="display: block; width: 50%; margin-right: auto; margin-left: auto; position: relative;">
+                <form class="example" action="board.php" method="post">    <!-- form 태그에서 action을 board.php로 method를 post형식으로 수정함-->
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
             </div>
         </div>
 
-        <hr class="seperator">
+        <!--<hr class="seperator fifty-percent">-->
 
+        <!-- Example Boardz element. -->
+        <div class="boardz centered-block beautiful">
+            <?php
+            //2번째 php 시작 부분에서 실제적인 데이터를 출력하는 부분.
+            $count = ($records % 3);        //레코드의 수를 3으로 나눈 나머지를 구해서 각 줄에 몇개씩 추가로 출력할 것인지를 구함
+            if($records > 3){
+                $column = 3;
+            }else {                         //레코드의 숫자가 3 이하인 경우 레코드의 숫자에 맞춰 ul태그의 갯수로 맞춤.
+                $column = $records;
+            }
+            for($i = 0 ; $i < $column ; $i++){
+                echo "<ul>";
+                for($j = 1; $j <($records / 3) ; $j++){         //레코드의 숫자를 3으로 나눠서 몇개씩 각 줄에 맞춰 통일적으로 출력할 것인지 맞춤
+                    if($row = mysql_fetch_array($result)){      //mysql_fetch_array()를 이용하여 DB의 원하는 데이터를 출력 표현.
+                        echo "<li>\n";
+                        echo "<h1>$row[title]</h1>\n";          //title은 <h1> 태그를 이용하여 크게 표현
+                        echo "$row[contents]\n";                //contents는 단순히 문자열로 출력함
+                        echo "<img src='$row[image_url]' alt='demo image'>\n";      //image_url은 img태그를 사용하여 출력함.
+                        echo "</li>\n";
+                    }
+                }
+                if($count > 0 || (($records %3 )== 0)){      //추가로 출력할 개수가 1개 이상이거나 레코드의 숫자가 3의 배수이면 추가로 1개씩을 출력함.
+                    if($row = mysql_fetch_array($result)){
+                        echo "<li>\n";
+                        echo "<h1>$row[title]</h1>\n";
+                        echo "$row[contents]\n";
+                        echo "<img src='$row[image_url]' alt='demo image'>\n";
+                        echo "</li>\n";
+                    }
+                    $count--;
+                }
+                echo "</ul>";
+            }
+            ?>
+        </div>
     </div>
+
+    <hr class="seperator">
+
+</div>
 </body>
 </html>
