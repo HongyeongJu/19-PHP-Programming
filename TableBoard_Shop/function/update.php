2900cdec7958c08ddb5b4ccbcf19905ce95b5d27
Hong Yeong Ju <abc123472000@naver.com>
2019-04-05 22:35:01 +0900

    UPDATE myPractice

--- TableBoard_Shop/function/update.php
+++ TableBoard_Shop/function/update.php
@@ -14,7 +14,7 @@ $db_con = mysql_select_db("hyj_db", $connect);
 $sql = "update tableboard_shop set date ='$_POST[date]' , order_id = '$_POST[order_id]' , name = '$_POST[name]' , price = '$_POST[price]' , quantity = '$_POST[quantity]'
 where num = '$_GET[num]';";
 
-echo $sql;
+//echo $sql;
 $result = mysql_query($sql,$connect);
 if($result){
 }
